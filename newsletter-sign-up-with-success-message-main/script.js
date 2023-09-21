488b499932c220c4bf88aa025084c49b9ba3a816
Atr3yus <robson_alado@hotmail.com>
2023-09-21 18:26:02 -0300

    acrescentado messagem de falha

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,16 +1,30 @@
-function enviar(){
-    //validaEmail();
-    window.location="sucess.html";
-}
+const form = document.querySelector("#form");
+const emailBox = document.querySelector("#email-box");
+const invalidMail = document.querySelector("#invalidMail");
+const inpu = document.querySelector("#email-box");
+
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    if(emailBox.value === ""){
+        
+        invalidMail.style.display = "block";
+        inpu.style.backgroundColor = "rgba(231, 67, 67, 0.2)";
+        
+
+        return;
+    }else{
+        window.location="sucess.html";
+    }
+})
 
 function validaEmail(email){
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
 
-
-
-
 function retorna(){
     window.location="index.html"
 }
+
